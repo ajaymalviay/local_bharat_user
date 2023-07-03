1852a3f851ce4a5a3edf30b6f3d7da09b61008d4
AjayMalviya <ajay.malpahwizz@gmail.com>
2023-07-03 17:39:29 +0100

    Initial commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,19 @@
+final String appName = 'Local Bharat';
+
+final String packageName = 'com.localBharat.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.localBharat.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = 'com.localBharat.user';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+final String baseUrl = 'https://developmentalphawizz.com/local_bharat/app/v1/api/';
+final String imageUrl = 'https://developmentalphawizz.com/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
